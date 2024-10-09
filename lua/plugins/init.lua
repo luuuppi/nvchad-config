29493959eae88dc0bad905512692664b3b498191
luppi <Mikhail.Yakushinale@Gmail.com>
2024-10-09 17:09:29 +0700

    feat: add JSX comment support

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,4 +59,22 @@ return {
       },
     },
   },
+
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring", "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
+
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      enable_autocmd = false,
+    },
+  },
 }
