1736104d908388e442d54614af595cb7c82a852c
luppi <Mikhail.Yakushinale@Gmail.com>
2025-02-16 01:13:54 +0700

    feat(telescope): show show dotfiles in find_files()

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,10 @@ return {
         },
         file_ignore_patterns = {
           "node_modules",
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
         },
       },
     },
